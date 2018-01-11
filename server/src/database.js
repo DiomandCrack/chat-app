1096d7349e08ac5eb90fb475c1063253d5fc343a
DiamondCrack <zk05161219@gmail.com>
2018-01-11 18:44:30 +0800

    success connecting to MongoDb

--- server/src/database.js
+++ server/src/database.js
@@ -1,14 +1,22 @@
 const MongoClient = require('mongodb').MongoClient
 const URL = 'mongodb://localhost:27017/chatapp'
 class Database {
-    constructor() {
-
-    }
     connect() {
-        MongoClient.connect(URL, (err, db) => {
-            console.log('Connecting to database with err', err)
-        });
-    }
+            return new Promise((resolve, reject) => {
+                MongoClient.connect(URL, (err, db) => {
+                    /*   if (err) {
+                          return reject(err)
+                      }
+                      return resolve(db) */
+                    return err ? reject(err) : resolve(db)
+                })
+            })
+        }
+        // connect(cb = () => {}) {
+        //     MongoClient.connect(URL, (err, db) => {
+        //         return cb(err, db)
+        //     });
+        // }
 }
 
 module.exports = Database
--- server/src/index.js
+++ server/src/index.js
@@ -5,8 +5,11 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const webSocketServer = require('uws').Server;
 
-const Model = require('./models/index')
-const AppRouter = require('./app-router')
+//require database
+const Database = require('./database')
+
+const Model = require('./models');
+const AppRouter = require('./app-router');
 
 const PORT = 3001;
 const app = express();
@@ -16,19 +19,37 @@ app.use(morgan('dev'));
 
 app.use(cors({
     exposedHeaders: '*'
-}))
+}));
 
 app.use(bodyParser.json({
     limit: '50mb'
-}))
+}));
 
 
 app.wss = new webSocketServer({
     server: app.server
-})
-
+});
+//connect to Mongo database 
+//promise
+new Database().connect().then((db) => {
+        console.log("Successful connected to database")
+        app.db = db;
+    }).catch((err) => {
+        throw (err)
+    })
+    /* 
+    //callback function ver
+    new Database().connect((err, db) => {
+        console.log('Connecting to database with err', err)
+        if (err) {
+            throw (err);
+        }
+        console.log("successful connect to database")
+        app.db = db;
+    }); */
+    //end to Mongo
 app.models = new Model(app);
-app.routers = new AppRouter(app)
+app.routers = new AppRouter(app);
 
 
 app.server.listen(process.env.PORT || PORT, () => {
